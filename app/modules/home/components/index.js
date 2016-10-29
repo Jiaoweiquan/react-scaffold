9a70c8b814ac15e30bf4c331d90f96943582ee82
Jiaoweiquan <jiaowei13@live.cn>
2016-10-29 13:38:27 +0800

    目前样式看起来还不错

--- app/modules/home/components/index.js
+++ app/modules/home/components/index.js
@@ -17,6 +17,12 @@ class Home extends React.Component {
     router: React.PropTypes.object.isRequired
   }
 
+  constructor(props) {
+    super(props)
+    // this.onSelectItem = this.onSelectItem.bind(this)
+    'something here'
+  }
+
   componentWillMount() {
   }
 
@@ -27,14 +33,29 @@ class Home extends React.Component {
   componentDidUpdate(prevProps, prevState) {
   }
 
+  onSelectItem(log) {
+    console.log('you have select', log)
+  }
+
   render() {
     const { flowerLog } = this.props.home
     return (
       <div className={styles.container}>
         <Header text="送花排行榜" />
-        {flowerLog.map(log => {
-          return <ListItem key={log.uid} userName={'anoymous' || log.uid} count={log.amount} imgSrc={avatar} />
-        })}
+        <ul className={styles.log_list}>
+          {
+            flowerLog.map(log => {
+              return (
+                <ListItem
+                  key={log.uid}
+                  onClick={this.onSelectItem.bind(this, log)}
+                  userName={'anoymous' || log.uid}
+                  count={log.amount}
+                  imgSrc={avatar} />
+              )
+            })
+          }
+        </ul>
       </div>
     )
   }
--- app/modules/listItem/index.js
+++ app/modules/listItem/index.js
@@ -2,8 +2,12 @@ import * as React from 'react'
 import style from './item.css'
 
 export default function ListItem({userName, count, imgSrc}) {
-  return (<li className={style.item}>
-    <img src={imgSrc} />
-    <span>{userName}</span><i>{count}</i>
-  </li>)
+  return (
+    <li className={style.item}>
+      <div className={style.container}>
+        <img src={imgSrc} />
+        <span>{userName}</span><i className={style.count}>{count}</i>
+      </div>
+    </li>
+  )
 }
